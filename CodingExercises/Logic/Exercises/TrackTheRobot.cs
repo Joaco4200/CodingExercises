b46a1b9426c18eb328df6df1408ea37518400836
Joaquin Scarone <joaquinscarone@192.168.1.100>
2025-03-13 18:52:08 -0300

    First commit

---
+++ CodingExercises/Logic/Exercises/TrackTheRobot.cs
@@ -0,0 +1,33 @@
+﻿namespace Logic;
+
+public class TrackTheRobot
+{
+    public static int[] TrackRobot(string[] instructions){
+        int x=0;
+        int y=0;
+
+        if(instructions==null){
+            return new int[] {0,0};
+        }
+
+        foreach(var instruction in instructions){
+            string[] parts=instruction.Split(' ');
+            string direction=parts[0];
+            int distance=int.Parse(parts[1]);
+            if(direction== "right"){
+                x+= distance;
+            }
+            else if(direction=="left"){
+                x-= distance;
+            }
+            else if (direction=="up"){
+                y+= distance;
+            }
+            else if(direction=="down"){
+                y-= distance;
+            }
+        }
+        
+        return new int[]{x, y};
+    }
+}
---
+++ Testing/Tests/TrackTheRobotTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests;
+using Xunit;
+using Logic;
+
+public class TrackTheRobotTests
+{
+    [Fact]
+    public void defaultcordinatesResult()
+    {   
+        var result= TrackTheRobot.TrackRobot(new string[] {"right 37","left 62","up 60","down 10"});
+        Assert.Equal(new[] { -25, 50}, result);
+    }
+
+    [Fact]
+    public void cordinatesResult()
+    {   
+        var result= TrackTheRobot.TrackRobot(new string[] {"right 50","right 1000","up 30","down 15"});
+        Assert.Equal(new[] { 1050, 15}, result);
+    }
+
+    [Fact]
+    public void nullinstructionsTest()
+    {   
+        var result= TrackTheRobot.TrackRobot(new string[] {});
+        Assert.Equal(new[] {0,0}, result);
+    }
+}
